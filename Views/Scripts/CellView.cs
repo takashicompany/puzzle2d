1d16920678656c91c9fe78ae83120015de179c09
takashicompany <t@kashi.company>
2019-05-04 17:12:29 +0900

    viewを追加

---
+++ Views/Scripts/CellView.cs
@@ -0,0 +1,64 @@
+﻿namespace TakashiCompany.Game.Puzzle2d.Unity
+{
+	using UnityEngine;
+	using UnityEngine.UI;
+
+	[RequireComponent(typeof(CanvasGroup))]
+	public class CellView : MonoBehaviour
+	{
+		public delegate void UpdateViewDelegate(CellView viwe, ICell cell);
+
+		public static UpdateViewDelegate onUpdateView;
+
+		[SerializeField]
+		private Image _outLine;
+
+		public Image outLine { get { return _outLine; } }
+
+		[SerializeField]
+		private Image _background;
+
+		public Image background { get { return _background; } }
+
+		[SerializeField]
+		private Text _text;
+	
+		private ICell _cell;
+
+		private CanvasGroup _canvasGroup;
+
+
+		private void Awake()
+		{
+			_canvasGroup = GetComponent<CanvasGroup>();
+		}
+
+		public void Setup(ICell cell)
+		{
+			_cell = cell;
+		}
+
+		void Update()
+		{
+			if (_cell == null)
+			{
+				_canvasGroup.alpha = 0f;
+			}
+			else
+			{
+				_background.enabled = !_cell.IsEmpty();
+				_text.enabled = !_cell.IsEmpty();
+
+				if (!_cell.IsEmpty())
+				{
+					_text.text = _cell.ToString();
+				}
+
+				if (onUpdateView != null)
+				{
+					onUpdateView(this, _cell);
+				}
+			}
+		}
+	}
+}
---
+++ Views/Scripts/LogicView.cs
@@ -0,0 +1,38 @@
+﻿namespace TakashiCompany.Game.Puzzle2d.Unity
+{
+	using UnityEngine;
+	using UnityEngine.UI;
+
+	[RequireComponent(typeof(GridLayoutGroup))]
+	public class LogicView : MonoBehaviour
+	{
+		[SerializeField]
+		private CellView _cellViewPrefab;
+
+		private GridLayoutGroup _grid;
+
+		private ILogic _logic;
+
+		void Awake()
+		{
+			_grid = GetComponent<GridLayoutGroup>();
+		}
+
+		public void SetLogic(ILogic logic)
+		{
+			_logic = logic;
+
+			_grid.constraintCount = _logic.width;
+
+			for (int y = _logic.height - 1; y >= 0; y--)
+			{
+				for (int x = 0; x < _logic.width; x++)
+				{
+					var cell = _logic.GetCell(x, y);
+					var cellView = Instantiate<CellView>(_cellViewPrefab, this.transform);
+					cellView.Setup(cell);
+				}
+			}
+		}
+	}
+}
